57eb74a4f16decdeff10c7c0ba4436a0d1bd2f3b
azureus75007 <azureus75007@gmail.com>
2018-03-07 00:50:50 +0100

    first commit

---
+++ calculator/script/script.js
@@ -0,0 +1,13 @@
+
+
+function addFunc() {
+    var num1 = document.getElementById("num1");
+    var num2 = document.getElementById("num2");
+    var output = document.getElementById("output");
+    
+
+
+  
+   output.innerHTML = Number(num1.value) + Number(num2.value);
+
+}
---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+/*var task = ["take out garbage","read a book", "write javascript","go to workd"];
+var progress = ["done","in progress", "to do"];
+var question1 = prompt("which task 1-4");
+var question2 = prompt("status 1 = done 2 = ");  
+console.log (task[question1]);
+*/
